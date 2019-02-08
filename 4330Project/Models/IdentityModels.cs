6ec5eec8e4e690237eaf921a423c945a595fb24e
Claire Palmer <cpalm18@lsu.edu>
2019-02-07 22:15:33 -0600

    Connected to local database

--- 4330Project/Models/IdentityModels.cs
+++ 4330Project/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace _4330Project.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ProjectDBIndentityConnection", throwIfV1Schema: false)
         {
         }
 
