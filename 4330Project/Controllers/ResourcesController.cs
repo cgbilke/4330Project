c4d51ccc661f542f690853b3f31723f03fe807e5
cpalm18 <30815822+cpalm18@users.noreply.github.com>
2019-03-12 22:39:15 -0500

    Added Azure Model and Resources View

---
+++ 4330Project/Controllers/ResourcesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _4330Project.Models;
+
+namespace _4330Project.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        private AzureEntities db = new AzureEntities();
+
+        // GET: Resources
+        public ActionResult Index()
+        {
+            var resources = db.Resources.Include(r => r.AspNetUser);
+            return View(resources.ToList());
+        }
+
+        // GET: Resources/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resource resource = db.Resources.Find(id);
+            if (resource == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resource);
+        }
+
+        // GET: Resources/Create
+        public ActionResult Create()
+        {
+            ViewBag.user_id = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Resources/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,user_id,title")] Resource resource)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Resources.Add(resource);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.user_id = new SelectList(db.AspNetUsers, "Id", "Email", resource.user_id);
+            return View(resource);
+        }
+
+        // GET: Resources/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resource resource = db.Resources.Find(id);
+            if (resource == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.user_id = new SelectList(db.AspNetUsers, "Id", "Email", resource.user_id);
+            return View(resource);
+        }
+
+        // POST: Resources/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,user_id,title")] Resource resource)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(resource).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.user_id = new SelectList(db.AspNetUsers, "Id", "Email", resource.user_id);
+            return View(resource);
+        }
+
+        // GET: Resources/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resource resource = db.Resources.Find(id);
+            if (resource == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resource);
+        }
+
+        // POST: Resources/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Resource resource = db.Resources.Find(id);
+            db.Resources.Remove(resource);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
